475420bc24bff43b94b89c8eee365e73aeaaec65
YourFriendlyUniverse <leontomlol1919@gmail.com>
2025-12-04 01:07:38 -0500

    Completed Day 4 Part 2

--- 2025/Day04.py
+++ 2025/Day04.py
@@ -9,17 +9,30 @@ def can_access(x, y, grid):
                 if not (x_diff == 0 and y_diff == 0):
                     if grid[y + y_diff][x + x_diff] == '@':
                         adjacent_papers += 1
-                
-                
     return (adjacent_papers < 4)
-        
 
-with open('2025/input.txt', 'r') as file:
-    lines = [line.strip() for line in file]
-    total_accessible = 0
+def accessible_papers(lines):
+    list = []
     
     for y in range(len(lines)):
         for x in range(len(lines[0])):
             if can_access(x, y, lines):
-                total_accessible += 1
-print(total_accessible)
+                list.append([x, y])
+    return list
+
+with open('2025/input.txt', 'r') as file:
+    lines = [line.strip() for line in file]
+    total_accessible = accessible_papers(lines)
+    new_papers = len(total_accessible)
+    total_no_remove = len(total_accessible)
+    total = total_no_remove
+
+    while (new_papers != 0):
+        for x, y in total_accessible:
+            lines[y] = lines[y][:x] + "x" + lines[y][(x + 1):]
+        total_accessible = accessible_papers(lines)
+        new_papers = len(total_accessible)
+        total += len(total_accessible)
+
+print(f"Part 1: {total_no_remove}")
+print(f"Part 2: {total}")
